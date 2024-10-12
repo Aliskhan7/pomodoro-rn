04f8870b97e00653d8008cf7f81a7f595b13377d
aliskhangazamatov <ag@farcana.com>
2024-10-12 16:05:19 +0300

    add authProvider, useAuth

--- app/Providers/AuthProvider.tsx
+++ app/Providers/AuthProvider.tsx
@@ -1,17 +1,32 @@
-import React, { createContext, Dispatch, SetStateAction } from 'react'
-import { View } from 'react-native'
+import React, {
+	createContext,
+	Dispatch,
+	FC,
+	PropsWithChildren,
+	SetStateAction,
+	useState
+} from 'react'
 import { IUser } from '@/types/user.interface'
+import * as Splash from 'expo-splash-screen'
 
 export type TypeUserState = IUser | null
 
 interface IContext {
-	user: IUser
+	user: TypeUserState
 	setUser: Dispatch<SetStateAction<TypeUserState>>
 }
 
 export const AuthContext = createContext({} as IContext)
-const AuthProvider = () => {
-	return <View></View>
+
+let ignore = Splash.preventAutoHideAsync()
+
+const AuthProvider: FC<PropsWithChildren<unknown>> = ({ children }) => {
+	const [user, setUser] = useState<TypeUserState>(null)
+	return (
+		<AuthContext.Provider value={{ user, setUser }}>
+			{children}
+		</AuthContext.Provider>
+	)
 }
 
 export default AuthProvider
