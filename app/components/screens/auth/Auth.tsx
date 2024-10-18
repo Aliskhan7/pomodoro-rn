f7e508dfa6b8911bb3bace84da161ace30708297
aliskhangazamatov <ag@farcana.com>
2024-10-18 18:37:28 +0300

    auth

--- app/components/screens/auth/Auth.tsx
+++ app/components/screens/auth/Auth.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
-import { View } from 'react-native'
-import { useForm } from 'react-hook-form'
+import { SubmitHandler, useForm } from 'react-hook-form'
 import { IAuthFormData } from '@/types/auth.interface'
+import { useAuth } from '@/hooks/useAuth'
+import { Keyboard, Text, TouchableWithoutFeedback, View } from 'react-native'
+import Loader from '@/components/ui/layout/Loader'
 
 const Auth = () => {
 	const [isReg, setIsReg] = useState(false)
@@ -10,7 +12,29 @@ const Auth = () => {
 		mode: 'onChange'
 	})
 
-	return <View></View>
+	const { setUser } = useAuth()
+
+	const onSubmit: SubmitHandler<IAuthFormData> = data => {
+		setUser({
+			_id: '',
+			...data
+		})
+	}
+
+	const isLoading = false
+
+	return (
+		<TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+			<View className='items-center justify-center flex-1'>
+				<View className='w-3/4'>
+					<Text className='text-white text-xl text-center'>
+						{isReg ? 'Sign up' : 'Sign in'}
+					</Text>
+					{isLoading ? <Loader /> : <></>}
+				</View>
+			</View>
+		</TouchableWithoutFeedback>
+	)
 }
 
 export default Auth
